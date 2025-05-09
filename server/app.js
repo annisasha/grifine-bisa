34344da13c1a594e4194f73b62134a1ff1ff65b8
annisasha <annisashafirap@gmail.com>
2025-05-09 10:15:54 +0700

    cors add

--- server/app.js
+++ server/app.js
@@ -9,9 +9,16 @@ dotenv.config();
 
 const app = express();
 
+// Konfigurasi CORS dengan opsi yang lebih spesifik
+const corsOptions = {
+  origin: 'https://grifine.vercel.app',  // Ganti dengan URL frontend kamu
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors(corsOptions)); // Gunakan CORS dengan konfigurasi
+app.use(express.json());     // Untuk parsing request body
 
 // Database Connection
 connectDB();
@@ -21,4 +28,3 @@ app.use("/api", authRoutes);
 app.use("/api", chatRoutes);
 
 export default app;
-
