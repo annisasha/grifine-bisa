d95553371d60be464034ba909f4f8de9a5aea5a1
annisasha <annisashafirap@gmail.com>
2025-05-20 20:56:46 +0700

    conversation update

--- server/models/Conversation.js
+++ server/models/Conversation.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
 
-const ConversationSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  title: { type: String, required: true },
-  messages: [
-    {
-      sender: { type: String, enum: ["user", "bot"], required: true },
-      text: { type: String, required: true },
-      timestamp: { type: Date, default: Date.now },
-    },
-  ]
-}, {
-  timestamps: true
-});
+const ConversationSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true },
+    messages: [
+      {
+        sender: { type: String, enum: ["user", "bot"], required: true },
+        text: { type: String, required: true },
+        timestamp: { type: Date, default: Date.now },
+      },
+    ]
+  },
+  { timestamps: true } // ✅ ini tempat yang benar
+);
+
 
 export default mongoose.model("Conversation", ConversationSchema);
