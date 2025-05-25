ba1dcf02b53a844299d6c7ecbb46dd71afb28b78
annisasha <annisashafirap@gmail.com>
2025-05-25 16:23:16 +0700

    date update 2

--- server/config/db.js
+++ server/config/db.js
@@ -1,17 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI);
-//     console.log('MongoDB Connected');
-//   } catch (err) {
-//     console.error(err.message);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
@@ -19,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    console.log("MONGODB_URI dari .env:", process.env.MONGODB_URI);  // Cek apakah .env sudah terbaca
+    console.log("MONGODB_URI dari .env:", process.env.MONGODB_URI); 
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('✅ MongoDB connected');
   } catch (err) {
